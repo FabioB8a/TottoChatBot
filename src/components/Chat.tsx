49d1abc263cd29391d3e4471900e4f86969a1549
FabioB8a <fabiobuitra@gmail.com>
2023-12-07 17:42:14 -0500

    Update: Create Accordion in ChatBot

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,7 +1,27 @@
+"use client";
+
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger} from '@/app/components/ui/accordion'
 import { FC } from 'react'
 
 const Chat: FC= () =>{
-    return <div>Chat</div>
+    return <Accordion
+    type='single'
+    collapsible
+    className='relative bg-white z-40 shadow'>
+        <AccordionItem value='item-1'>
+        <div className='fixed right-8 w-80 bottom-8 bg-white border border-gray-200 rounded-md overflow-hidden'>
+            <div className='w-full h-full flex flex-col'>
+            <AccordionTrigger className='px-6 border-b border-zinc-300'>
+
+            </AccordionTrigger>
+            <AccordionContent>
+              <div className='flex flex-col h-80'>
+              </div>
+            </AccordionContent>
+            </div>
+        </div>
+        </AccordionItem>
+  </Accordion>
 }
 
 export default Chat
