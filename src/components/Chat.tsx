8ebc6b74078cddc072e03c0f7bb26de58b9e4924
FabioB8a <fabiobuitra@gmail.com>
2023-12-07 17:45:57 -0500

    Add: Implementation of ChatHeader

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -2,6 +2,7 @@
 
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger} from '@/app/components/ui/accordion'
 import { FC } from 'react'
+import ChatHeader from './ChatHeader';
 
 const Chat: FC= () =>{
     return <Accordion
@@ -12,7 +13,7 @@ const Chat: FC= () =>{
         <div className='fixed right-8 w-80 bottom-8 bg-white border border-gray-200 rounded-md overflow-hidden'>
             <div className='w-full h-full flex flex-col'>
             <AccordionTrigger className='px-6 border-b border-zinc-300'>
-
+            <ChatHeader/>
             </AccordionTrigger>
             <AccordionContent>
               <div className='flex flex-col h-80'>
