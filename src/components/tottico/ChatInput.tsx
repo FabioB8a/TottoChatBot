006766663eea36b81a77cce6731c986af12f60cf
FabioB8a <fabiobuitra@gmail.com>
2023-12-07 19:19:50 -0500

    Update: ChatInput and ADD Validators

--- src/components/tottico/ChatInput.tsx
+++ src/components/tottico/ChatInput.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { cn } from '@/lib/utils'
+import { useMutation } from '@tanstack/react-query';
+import { nanoid } from 'nanoid';
 import { FC, HTMLAttributes, useContext, useRef, useState } from 'react'
 import TextareaAutosize from 'react-textarea-autosize'
 
@@ -9,11 +11,40 @@ interface ChatInputProps extends HTMLAttributes<HTMLDivElement> {}
 const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
 
     const [input, setInput] = useState<string>('')
+
+    const { mutate: sendMessage } = useMutation({
+
+        mutationFn: async () => {
+          const response = await fetch('/api/message', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ messages: 'Tottico' }),
+          })
+    
+          return response.body
+        },
+        onSuccess: async (stream) => {
+          console.log("Bien")
+          }
+        })
     
     return (
         <div {...props} className={cn('border-t border-zinc-1000', className)}>
             <div className='relative mt-1 mr-5 ml-5 flex-1 overflow-hidden rounded-lg outline-none'>
                 <TextareaAutosize
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter' && !e.shiftKey){
+                            e.preventDefault()
+
+                            const message= {
+                                id: nanoid(),
+                                isUserMessage: true,
+                                text: input,
+                            }
+                        }
+                    }}
                     rows={2}
                     maxRows={4}
                     value={input}
