16c4373fb5ed43487b03948fe3ad01264cfcbe76
FabioB8a <fabiobuitra@gmail.com>
2023-12-12 23:36:05 -0500

    ADD: Data

--- src/helpers/constants/items-data.ts
+++ src/helpers/constants/items-data.ts
@@ -25,6 +25,18 @@ export const items = `
     <desc>Maleta De Viaje Bazy 360° Grande Blanca Bodega Rígida y Alta Resistencia</desc>
     <precio>$319.900</precio>
     </item>
+    </item>
+    <url>https://co.totto.com/morral-summit-20-et04sum025-2320-v3a/p</url>
+    <desc>Morral Summit 20 L 15-30 Litros Verde - El Morral Summit 20 es mucho más que un simple morral; es tu compañero fiel en cada paso del camino. Sus características excepcionales te permiten viajar con confianza y estilo. Es color verde y tiene capacidad en litros de 15-30 litros
+    Color: Verde Capacidad: 24.51 Lt. Dimensiones: Alto: 51 cm x Ancho: 31 cm x Profundo: 15.5 cm Tamaño: Grande</desc>
+    <precio>$ 249.900</precio>
+    </item>
+    </item>
+    <url>https://co.totto.com/mochila-outdoor-de-viaje-summit-20-et04sum025-2310-n01/p</url>
+    <desc>Morral De Viaje Summit 20 Outdoor El Morral Summit 20 es mucho más que un simple morral; es tu compañero fiel en cada paso del camino. Sus características excepcionales te permiten viajar con confianza y estilo. Es color negro y tiene capacidad en litros de 15-30 litros
+    Color: Negro Categoría: morrales Capacidad: 24 Dimensiones: Alto: 51cm x Ancho: 31cm x profundo: 15.5cm Material: Exterior: Poliamida=60%</desc>
+    <precio>$ 249.900</precio>
+    </item>
 </content>
 </url>
 <url>
@@ -53,6 +65,54 @@ export const items = `
     Tamaño: pequeño
     </desc>
     <precio>$ 199.900</precio>
+    </item>
+    </item>
+    <url>https://co.totto.com/bolsa-de-viaje-active-s-et05act025-2310-m89/p</url>
+    <desc>Tula De Viaje Active Pequeña ¡Siempre listo para cualquier reto! La Tula de Viaje Active Pequeña es perfecta para hacer deporte o para llevar a tus viajes el fin de semana. Es práctica, resistente y anti rasgado; también cuenta con correa removible y ajustable para llevar al hombro fácilmente, argollas para candado en la cremallera y kissing slider para mayor seguridad.
+    Color: Morado Género: Mujer Categoría: tulas Capacidad: 22 Dimensiones: Alto: 25cm x Ancho: 47cm x profundo: 19cm Material: Exterior: Poliester=100% Tamaño: pequeño</desc>
+    <precio>$ 249.900</precio>
+    </item>
+    </item>
+    <url>https://co.totto.com/bolsa-de-viaje-active-wheels-et05act001-2310-n01/p</url>
+    <desc>Tula De Viaje Active Wheels Tula Active Wheels M, tula de viaje con exclusivo diseño y se puede llevar cruzada, de mano o de ruedas. Cuenta con bolsillos internos en malla con cremallera, tiene dos ruedas que dan mayor flexibilidad y un soporte en la parte frontal que brinda mayor firmeza. De tamaño mediano con una gran capacidad y correas ajustables al hombro.
+    Color: Negro Categoría: tulas Capacidad: 61 Dimensiones: Alto: 32cm x Ancho: 64cm x profundo: 30cm Material: Exterior: Poliester=95% Tamaño: Mediano</desc>
+    <precio>$ 249.900</precio>
+    </item>
+</content>
+</url>
+<url>
+<loc>https://items.toscrape.com/catalogue/category/items/mystery_3/index.html</loc>
+<desc>TOTTO Travel colección ACCESORIOS</desc>
+<content>
+    <item>
+    <url>https://co.totto.com/canguro-de-seguridad-security-at50ind002-2320-n01/p<url>
+    <desc>Canguro Negro Invisible Security Descubre nuestro canguro invisible con sistema RFID, el complemento ideal para proteger tu información personal en tus viajes. Fabricado con materiales de primera calidad, su frontal de poliéster y su parte posterior transpirable garantizan una experiencia cómoda y segura al llevarlo directamente sobre la piel. Este versátil canguro cuenta con dos bolsillos de fácil acceso, equipados con cremalleras y pullers metálicos para mayor seguridad.
+    Color: Negro Capacidad: 0.36 Lt. Dimensiones: Alto: 13 cm x Ancho: 28 cm x Profundo: 1 cm Tamaño: pequeño</desc>
+    <precio>$ 49.900</precio>
+    </item>
+    <item>
+    <url>https://co.totto.com/almohada-memory-foam-pylot-at79ind001-2320-n01/p<url>
+    <desc>Almohada De Viaje Memory Foam Negra Pylot ¡Viaja con total comodidad y previene el dolor cervical con nuestra almohada de viaje de memory foam! Fabricada en poliéster con textura suave, esta almohada te brindará el soporte y la suavidad que necesitas durante tus desplazamientos. La tecnología Memory Foam se adapta perfectamente a la forma de tu cuello, proporcionando un ajuste personalizado para mayor confort.
+    Color: Negro Dimensiones: Alto: 10 cm x Ancho: 32.5 cm x Profundo: 29 cm</desc>
+    <precio>$ 49.900</precio>
+    </item>
+    <item>
+    <url>https://co.totto.com/porta-pasaporte-complyt-at87ind003-2320-n01/p<url>
+    <desc>Porta Pasaporte Complyt Negro ¡Viaja con tranquilidad y estilo con nuestro Porta Pasaporte! Diseñado con cierre en cremallera para una seguridad adicional, este práctico accesorio tiene diferentes compartimentos internos que te permiten organizar documentos importantes como pasaportes, pases de abordar, tarjetas, dinero y otros elementos esenciales para tu viaje. Con una cómoda manija para sujetarlo en la mano, tendrás todo lo que necesitas al alcance en cada paso del camino.
+    Color: Negro Dimensiones: Alto: 15 cm x Ancho: 13 cm x Profundo: 1.2 cm</desc>
+    <precio>$ 49.900</precio>
+    </item>
+    <item>
+    <url>https://co.totto.com/organizador-de-bano-viajero-all-in-at67ind005-2320-p12/p<url>
+    <desc>Organizador De Viaje All In Rosado o Negro Descubre el organizador de viaje perfecto para simplificar tus viajes. Compacto y altamente funcional, este organizador de baño es tu aliado ideal para mantener tus artículos personales organizados durante tus desplazamientos. Su diseño inteligente incluye un gancho que te permite colgarlo en puertas, percheros o ganchos, brindándote fácil acceso a tus pertenencias sin ocupar espacio adicional.
+    Color: Rosado</desc>
+    <precio>$ 59.900</precio>
+    </item>
+    <item>
+    <url>https://co.totto.com/candado-poxy-at55ind003-2320-g86/p<url>
+    <desc>Candado Para Maleta Poxy Gris ¡Protege tus pertenencias mientras viajas! Este candado es la opción ideal para garantizar la seguridad de tu equipaje gracias a su sistema TSA. Con un sistema de combinación de tres dígitos, podrás configurar una clave personalizada y mantener tus pertenencias a salvo en todo momento.
+    Color: Gris Dimensiones: Alto: 9 cm x Ancho: 3.5 cm x Profundo: 1 cm</desc>
+    <precio>$ 49.900</precio>
     </item>
 </content>
 </url>
