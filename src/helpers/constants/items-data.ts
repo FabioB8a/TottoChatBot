b2db8c48e5a47b957f7f7d0a815b6e59d594c8df
FabioB8a <fabiobuitra@gmail.com>
2023-12-12 22:46:28 -0500

    ADD: Items-data

--- src/helpers/constants/items-data.ts
+++ src/helpers/constants/items-data.ts
@@ -12,13 +12,13 @@ export const items = `
 <desc>TOTTO Travel colección MALETAS</desc>
 <content>
     <item>
-    <url>https://co.totto.com/viaje?initialMap=c&initialQuery=viaje&map=category-1,category-2&order=OrderByReleaseDateDESC&page=3&query=/viaje/maletas&searchState</url>
+    <url>https://co.totto.com/maleta-viaje-360-travel-lite-m-et17tvl002-2320-n01/p?gad_source=1&gclid=CjwKCAiApuCrBhAuEiwA8VJ6JrBpwe_GKfiX-nQfl6BKsOEEGPktKIRbhINEIgHxLHEi9rYdZhKTVRoCn60QAvD_BwE</url>
     <desc>Maleta De Bodega Travel Lite 23 Kg Negra Ultra Liviana Producto Viaje Seguridad Travel Sentry Argollas para Candado Elaborado con 18 botellas plásticas</desc>
     <precio>$429.000</precio>
     </item>
     <item>
-    <url>https://items.toscrape.com/catalogue/full-moon-over-noahs-ark-an-odyssey-to-mount-ararat-and-beyond_811/index.html</url>
-    <desc>Maleta De Viaje Archer 360° Expandible</desc>
+    <url>https://co.totto.com/maleta-viaje-360-archer-et17sky006-2220-m50/p?uam=true&mobile=4&gad_source=1&gclid=CjwKCAiApuCrBhAuEiwA8VJ6JtA2hTHZ43qvrg-uIQi20SYISECbcKO1HRMB9lpKCWZWgXwVtl3TIBoCXmsQAvD_BwE</url>
+    <desc>Maleta De Viaje Archer 360° Expandible Morado</desc>
     <precio>$559.900</precio>
     </item>
     <url>https://co.totto.com/maleta-viaje-360-bazy-l-et17baz003-2220-b04/p</url>
@@ -32,14 +32,27 @@ export const items = `
 <desc>TOTTO Travel colección TULAS</desc>
 <content>
     <item>
-    <url>https://co.totto.com/viaje?initialMap=c&initialQuery=viaje&map=category-1,category-2&order=OrderByReleaseDateDESC&query=/viaje/tulas&searchState</url>
+    <url>https://co.totto.com/tula-active-m-et05act026-2320-n01/p/<url>
     <desc>Tula Active M Negra</desc>
     <precio>$129.900</precio>
     </item>
     <item>
-    <url>https://co.totto.com/viaje?initialMap=c&initialQuery=viaje&map=category-1,category-2&order=OrderByReleaseDateDESC&query=/viaje/tulas&searchState</url>
-    <desc>Tula Active L Verde Especial para cabina</desc>
+    <url>https://co.totto.com/tula-active-l-et05act027-2320-4du/p?skuId=47875</url>
+    <desc>Tula Active L Verde Especial para cabina Color: Verde 
+    Capacidad: 54.81 Lt. 
+    Dimensiones: Alto: 34 cm x Ancho: 62 cm x Profundo: 26 cm
+    Tamaño: Grande</desc>
     <precio>$179.900</precio>
+    </item>
+    <item>
+    <url>https://co.totto.com/tula-cobred-et05dat001-2320-zo0/p<url>
+    <desc>Tula Cobred S Azul/Naranja: Esta tula Cobred es perfecta gracias a su portabilidad y compactibilidad, a su vez es ligera para cargar a todas partes gracias a sus dimensiones de 41 x 26 x 22.5 y a que es plegable y reducirá su tamaño. Está hecha en material disponible para el desgaste diario. Fácil de usar y limpiar.
+    Color: Azul
+    Capacidad: 23.98 Lt.
+    Dimensiones: Alto: 26 cm x Ancho: 41 cm x Profundo: 22.5 cm
+    Tamaño: pequeño
+    </desc>
+    <precio>$ 199.900</precio>
     </item>
 </content>
 </url>
